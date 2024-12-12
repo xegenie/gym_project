effed538dac1f4a972bd8aa599a3890d02facc2a
johe01 <jhe0598@gmail.com>
2024-12-12 18:33:03 +0900

    일단 comment 수정 등록까지

--- gym/src/main/java/com/gym/gym/domain/Comment.java
+++ gym/src/main/java/com/gym/gym/domain/Comment.java
@@ -2,12 +2,16 @@ package com.gym.gym.domain;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
 public class Comment {
 
     private int no;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX", timezone = "DEFAULT")
     private Date commentDate;
     private int trainerNo;
     private int userNo;
